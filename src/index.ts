5c6911ece95ca94d9383386de7534b3097c8a067
takeyu1013 <yuto1013@gmail.com>
2021-11-30 23:26:10 +0900

    ✨ Add to return user list

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post(`/user`, async (req, res) => {
+  const result = await prisma.user.create({
+    data: {
+      ...req.body,
+    },
+  });
+  res.json(result);
+});
+
+app.get(`/users`, async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json({ users });
+  console.log(users);
+});
+
+const server = app.listen(3001, () =>
+  console.log("🚀 Server ready at: http://localhost:3001")
+);
