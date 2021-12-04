968f879220a36d0e597aaa886ba819e81d119e62
takeyu1013 <yuto1013@gmail.com>
2021-12-04 15:35:57 +0900

    ➕ Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post(`/user`, async (req, res) => {
   const result = await prisma.user.create({
